e1e9783e6df0b30be13ecbfcb85af98c1b85c36c
albihasani94 <albihasani94@gmail.com>
2019-09-17 16:04:58 +0200

    initial commit

---
+++ src/main/java/org/example/app/greeting/GreetingController.java
@@ -0,0 +1,13 @@
+package org.example.app.greeting;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class GreetingController {
+
+    @RequestMapping("/hello")
+    public String sayHello() {
+        return "Hello";
+    }
+}
